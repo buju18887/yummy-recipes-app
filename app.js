1a27fbed3f4f11ba6055a108670eb2e89eb21392
unknown <luqmantazmid123@gmail.com>
2023-01-15 22:19:58 +0300

    added dependencies

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('Designs'))
 
 app.listen(5500, () => {
-    console.log('Server is listening on port 5500');
+  console.log('Server is listening on port 5500')
 })
