c91a80fb957dbac578573cc254e2394e3e826a29
unknown <luqmantazmid123@gmail.com>
2023-01-16 14:16:49 +0300

    Setup express js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.use(express.static('Designs'))
+app.use(express.static('./Designs'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './Designs/homepage.html'))
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('Resource not found')
+})
 
 app.listen(5500, () => {
-  console.log('Server is listening on port 5500')
+  console.log('Server is listening on port 5500...')
 })
